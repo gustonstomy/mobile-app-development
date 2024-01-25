dd8634d70bdba3ae93096b739775fdd24450a265
gustonstomy <gustomstomy@gmail.com>
2024-01-25 09:59:38 +0000

    upd

--- Ecomerce/lib/profile.dart
+++ Ecomerce/lib/profile.dart
@@ -10,6 +10,140 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(250),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        "assets/images/bt.jpg",
+                      ))),
+            ),
+            Positioned(
+              right: 30,
+              top: 110,
+              child: Container(
+                height: 250,
+                width: 350,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(color: Colors.grey, blurRadius: 10)
+                    ],
+                    borderRadius: BorderRadius.circular(20)),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: 0,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.menu_open_outlined)),
+                    ),
+                    const Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Janny Walker",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "New Zeland",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    )),
+                    Positioned(
+                      bottom: 20,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 40,
+                        ),
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width * 0.65,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  const Text(
+                                    "PURCHASED",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.sizeOf(context).height *
+                                        0.01,
+                                  ),
+                                  const Text(
+                                    "89",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const Text(
+                                    "FAVOURITE",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.sizeOf(context).height *
+                                        0.01,
+                                  ),
+                                  const Text(
+                                    "300",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const Text(
+                                    "VOUCHERS",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: MediaQuery.sizeOf(context).height *
+                                        0.01,
+                                  ),
+                                  const Text(
+                                    "8",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
